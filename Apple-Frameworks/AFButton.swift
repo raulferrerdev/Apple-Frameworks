80d19a75d219d9f691ac333ff02d2bad93159a31
RFG <raul.ferrer.dev@gmail.com>
2021-07-28 09:08:24 +0200

    Detail View

---
+++ Apple-Frameworks/AFButton.swift
@@ -0,0 +1,29 @@
+//
+//  AFButton.swift
+//  Apple-Frameworks
+//
+//  Created by RaulF on 28/7/21.
+//
+
+import SwiftUI
+
+struct AFButton: View {
+    
+    var title: String
+    
+    var body: some View {
+        Text(title)
+            .font(.title2)
+            .fontWeight(.semibold)
+            .frame(width: 280, height: 50)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+    }
+}
+
+struct AFButton_Previews: PreviewProvider {
+    static var previews: some View {
+        AFButton(title: "Test title")
+    }
+}
---
+++ Apple-Frameworks/FrameworkDetailView.swift
@@ -0,0 +1,51 @@
+//
+//  FrameworkDetailView.swift
+//  Apple-Frameworks
+//
+//  Created by RaulF on 28/7/21.
+//
+
+import SwiftUI
+
+struct FrameworkDetailView: View {
+    
+    var framework: Framework
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                Button {
+                    
+                } label: {
+                    Image(systemName: "xmark")
+                        .foregroundColor(Color(.label))
+                        .imageScale(.large)
+                        .frame(width: 44, height: 44)
+                }
+            }
+            .padding()
+            
+            Spacer()
+            
+            FrameworkTitleView(framework: framework)
+            
+            Text(framework.description)
+                .font(.body)
+                .padding()
+            
+            Spacer()
+            
+            Button {
+                
+            } label: {
+                AFButton(title: "Learn more")
+            }
+        }
+    }
+}
+
+struct FrameworkDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkDetailView(framework: MockData.sampleFramework)
+    }
+}
