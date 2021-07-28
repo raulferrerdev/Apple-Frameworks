a8176e4d57bcfef5564ccdc620a57407b575a4b8
raulferrerdev <raul.ferrer.dev@gmail.com>
2021-07-28 07:43:39 +0200

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by RaulF on 28/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
