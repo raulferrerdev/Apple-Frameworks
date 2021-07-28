e345f1d6c1e52f21ea56fd72b5351fcaa9ef577f
RFG <raul.ferrer.dev@gmail.com>
2021-07-28 07:56:53 +0200

    Intro & Project Stup

---
+++ Apple-Frameworks/FrameworkGridView.swift
@@ -0,0 +1,20 @@
+//
+//  FrameworkGridView.swift
+//  Apple-Frameworks
+//
+//  Created by RaulF on 28/7/21.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FrameworkGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkGridView()
+    }
+}
