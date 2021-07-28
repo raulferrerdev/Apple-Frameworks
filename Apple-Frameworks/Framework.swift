3b202bfffef4694768b0cac94ec614ed5031426a
RFG <raul.ferrer.dev@gmail.com>
2021-07-28 08:48:41 +0200

    Grid - The End

--- Apple-Frameworks/Framework.swift
+++ Apple-Frameworks/Framework.swift
@@ -7,7 +7,8 @@
 
 import Foundation
 
-struct Framework {
+struct Framework: Hashable, Identifiable {
+    var id = UUID()
     let name: String
     let imageName: String
     let urlString: String
--- Apple-Frameworks/FrameworkGridView.swift
+++ Apple-Frameworks/FrameworkGridView.swift
@@ -12,13 +12,16 @@ struct FrameworkGridView: View {
     let columns: [GridItem] = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
     
     var body: some View {
-        LazyVGrid(columns: columns) {
-            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
-            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
-            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
-            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
-            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
-            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(MockData.frameworks) { framework in
+                        FrameworkTitleView(framework: framework)
+
+                    }
+                }
+            }
+            .navigationTitle("🍎 Frameworks")
         }
     }
 }
@@ -26,25 +29,27 @@ struct FrameworkGridView: View {
 struct FrameworkGridView_Previews: PreviewProvider {
     static var previews: some View {
         FrameworkGridView()
+            .preferredColorScheme(.dark)
+            
     }
 }
 
 
 struct FrameworkTitleView: View {
     
-    let name: String
-    let imageName: String
+    let framework: Framework
     
     var body: some View {
         VStack {
-            Image(imageName)
+            Image(framework.imageName)
                 .resizable()
                 .frame(width: 90, height: 90)
-            Text(name)
+            Text(framework.name)
                 .font(.title2)
                 .fontWeight(.semibold)
                 .scaledToFit()
                 .minimumScaleFactor(0.6)
         }
+        .padding()
     }
 }
