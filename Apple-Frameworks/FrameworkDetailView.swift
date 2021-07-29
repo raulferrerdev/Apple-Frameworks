b383dbb037042848b5a2859c034f435d81eeda66
RFG <raul.ferrer.dev@gmail.com>
2021-07-29 08:43:02 +0200

    Integrating with UIKit - Introduction

--- Apple-Frameworks/FrameworkDetailView.swift
+++ Apple-Frameworks/FrameworkDetailView.swift
@@ -11,6 +11,8 @@ struct FrameworkDetailView: View {
     
     var framework: Framework
     @Binding var isShowingDetailView: Bool
+    @State private var isShowingSafariView = false
+    
     var body: some View {
         VStack {
             HStack {
@@ -35,12 +37,14 @@ struct FrameworkDetailView: View {
                 .padding()
             
             Spacer()
-            
             Button {
-                
+                isShowingSafariView = true
             } label: {
                 AFButton(title: "Learn more")
             }
+            .sheet(isPresented: $isShowingSafariView, content: {
+                SafariView(url: URL(string: framework.urlString) ?? URL(string: "www.apple.com")!)
+            })
         }
     }
 }
